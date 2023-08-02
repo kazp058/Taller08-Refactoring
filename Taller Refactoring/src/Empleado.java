3408bd0e35caccb64c23a2029d8a39f7779b61fb
kazp058@gmail.com <kazp058@gmail.com>
2023-08-02 15:47:37 -0500

    refactor: Replace Nested Conditional with Guard Clauses

--- Taller Refactoring/src/Empleado.java
+++ Taller Refactoring/src/Empleado.java
@@ -16,18 +16,16 @@ public class Empleado {
 
     public double calcularSalario() {
         double salarioTotal = salarioBase;
-        if (salarioBase>0) {
-            if (horasTrabajadas >= 0) {
-                // Horas trabajadas normales = 40;
-                if (horasTrabajadas > 40) {
-                    salarioTotal += (horasTrabajadas - 40) * 50; // Pago de horas extra
-                }
-            }else {
-                throw new IllegalArgumentException("Las horas trabajadas deben ser mayor o igual a 0");
-            }
-        } else {
-            throw new IllegalArgumentException("El salario debe ser mayor o igual a 0");
+        if(salarioBase < 0){
+            throw new IllegalArgumentException("El salario debe ser mayor o igual a 0"); 
         }
+        if(horasTrabajadas < 0){
+            throw new IllegalArgumentException("Las horas trabajadas deben ser mayor o igual a 0"); 
+        }
+        if(horasTrabajadas > 40){
+            salarioTotal += (horasTrabajadas - 40) * 50;
+        }
+
         switch (departamento) {
             case "Sistemas":
                 salarioTotal += 20;
